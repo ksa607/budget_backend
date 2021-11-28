2dcbe9202cbac58f2f7af649d2f723e86d784a33
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-11-28 10:19:26 +0100

    Add user validation

--- src/rest/_user.js
+++ src/rest/_user.js
@@ -1,24 +1,45 @@
+const Joi = require('joi');
 const Router = require('@koa/router');
 const userService = require('../service/user');
+const validate = require('./_validation');
 
 const getAllUsers = async (ctx) => {
   const users = await userService.getAll();
   ctx.body = users;
 };
+getAllUsers.validationScheme = null;
 
 const getUserById = async (ctx) => {
   const user = await userService.getById(ctx.params.id);
   ctx.body = user;
+};
+getUserById.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
 };
 
 const updateUserById = async (ctx) => {
   const user = await userService.updateById(ctx.params.id, ctx.request.body);
   ctx.body = user;
 };
+updateUserById.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
+  body: {
+    name: Joi.string().max(255),
+  },
+};
 
 const deleteUserById = async (ctx) => {
   await userService.deleteById(ctx.params.id);
   ctx.status = 204;
+};
+deleteUserById.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
 };
 
 /**
@@ -31,10 +52,10 @@ module.exports = function installUsersRoutes(app) {
     prefix: '/users',
   });
 
-  router.get('/', getAllUsers);
-  router.get('/:id', getUserById);
-  router.put('/:id', updateUserById);
-  router.delete('/:id', deleteUserById);
+  router.get('/', validate(getAllUsers.validationScheme), getAllUsers);
+  router.get('/:id', validate(getUserById.validationScheme), getUserById);
+  router.put('/:id', validate(updateUserById.validationScheme), updateUserById);
+  router.delete('/:id', validate(deleteUserById.validationScheme), deleteUserById);
 
   app
     .use(router.routes())
