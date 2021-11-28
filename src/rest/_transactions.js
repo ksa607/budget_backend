37dba260495616a48be411bc8d987d1101c382ad
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-11-28 10:12:35 +0100

    Add transaction validation

--- src/rest/_transactions.js
+++ src/rest/_transactions.js
@@ -1,9 +1,17 @@
+const Joi = require('joi');
 const Router = require('@koa/router');
 const transactionService = require('../service/transaction');
+const validate = require('./_validation.js');
 
 const getAllTransactions = async (ctx) => {
 	ctx.body = await transactionService.getAll();
 };
+getAllTransactions.validationScheme = {
+	query: Joi.object({
+		limit: Joi.number().positive().max(1000).optional(),
+		offset: Joi.number().min(0).optional(),
+	}).and('limit', 'offset'),
+ };
 
 const createTransaction = async (ctx) => {
 	const newTransaction = await transactionService.create({
@@ -14,9 +22,22 @@ const createTransaction = async (ctx) => {
 	ctx.body = newTransaction;
 	ctx.status=201;
 };
+createTransaction.validationScheme = {
+	body: {
+		amount: Joi.number().invalid(0),
+		date: Joi.date().iso().less('now'),
+		placeId: Joi.number().integer().positive(),
+		user: Joi.string(),
+  },
+};
 
 const getTransactionById = async (ctx) => {
 	ctx.body = await transactionService.getById(ctx.params.id);
+};
+getTransactionById.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
 };
 
 const updateTransaction = async (ctx) => {
@@ -26,10 +47,26 @@ const updateTransaction = async (ctx) => {
 		date: new Date(ctx.request.body.date),
 	});
 };
+updateTransaction.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
+  body: {
+    amount: Joi.number().invalid(0),
+    date: Joi.date().iso().less('now'),
+    placeId: Joi.number().integer().positive(),
+    user: Joi.string(),
+  },
+};
 
 const deleteTransaction = async (ctx) => {
 	await transactionService.deleteById(ctx.params.id);
 	ctx.status = 204;
+};
+deleteTransaction.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
 };
 
 /**
@@ -42,11 +79,11 @@ module.exports = (app) => {
 		prefix: '/transactions',
 	});
 
-	router.get('/', getAllTransactions);
-	router.post('/', createTransaction);
-	router.get('/:id', getTransactionById);
-	router.put('/:id', updateTransaction);
-	router.delete('/:id', deleteTransaction);
+	router.get('/', validate(getAllTransactions.validationScheme), getAllTransactions);
+	router.post('/', validate(createTransaction.validationScheme), createTransaction);
+	router.get('/:id', validate(getTransactionById.validationScheme), getTransactionById);
+	router.put('/:id', validate(updateTransaction.validationScheme), updateTransaction);
+	router.delete('/:id', validate(deleteTransaction.validationScheme), deleteTransaction);
 
 	app.use(router.routes()).use(router.allowedMethods());
 };
