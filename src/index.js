8a81a406d9b6a9ec5f0e7bf55de5efe909ff9110
Thomas Aelbrecht <thomasaelbrecht@live.com>
2022-12-13 15:25:24 +0100

    Print error before exiting Node

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ async function main() {
     process.on('SIGTERM', onClose);
     process.on('SIGQUIT', onClose);
   } catch (error){
+    console.error(error); // print the error before exiting
     process.exit(-1);
   }
 }
