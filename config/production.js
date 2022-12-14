81de81d2768de8440781ce8d381c7148e36194ad
ksa607 <karine.samyn@hogent.be>
2022-12-14 17:51:01 +0100

    production

--- config/production.js
+++ config/production.js
@@ -5,7 +5,7 @@ module.exports = {
     disabled: false,
   },
   cors: {
-    origins: ['https://frontendweb-budget.onrender.com'],
+    origins: ['https://budget-frontend-karine.onrender.com'],
     maxAge: 3 * 60 * 60, // 3h in seconds
   },
   database: {
